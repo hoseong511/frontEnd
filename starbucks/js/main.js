b70560a6638217a303b11cbde611f21229f09bc0
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-03-31 21:48:58 +0900

    update promotion slide

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -50,3 +50,24 @@ new Swiper('.notice-line .swiper-container', {
   autoplay : true,
   loop : true
 });
+
+new Swiper('.promotion .swiper-container', {
+  // horizontal은 기본값.
+  // direction : 'horizontal',
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides : true, // 1번 슬라이드가 가운데 보이기
+  loop : true,
+  autoplay : {
+    delay : 5000
+  },
+  pagination : {
+    el: '.promotion .swiper-pagination',
+    clickable : true
+  },
+  navigation : {
+    prevEl : '.promotion .swiper-prev',
+    nextEl : '.promotion .swiper-next'
+  }
+
+})
