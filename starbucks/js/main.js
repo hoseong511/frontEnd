23a702909055e7ce162d1a765d7fce4adfc67442
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-04 00:39:30 +0900

    update 다중요소슬라이드

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -111,8 +111,9 @@ const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({
-      triggerElement: spyEl
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
     })
-    .setClassToggle()
-    .addTo();
+    .setClassToggle(spyEl, 'show')    
+    .addTo(new ScrollMagic.Controller());
 });
