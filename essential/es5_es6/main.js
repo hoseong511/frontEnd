b90c8b25e45f76f08a68f27c5d34ad226b9b620d
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-12 23:16:32 +0900

    update 호이스팅~

--- essential/es5_es6/main.js
+++ essential/es5_es6/main.js
@@ -1,15 +1,18 @@
-// 즉시실행함수
-// IIFE, Immediately-invoked Function
+// 호이스팅(hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
 
-const a = 8
-function double(){
-  console.log(a * 2);
-}
+const a = 7
 
-(function () {
+double2();  //호이스팅에 의하여 실행됨.
+
+const double = function () {
   console.log(a*2);
-}());
+} // 이게 함수 표현임
 
-(function () {
-  console.log(a*2);
-})();
+double()
+
+function double2() {
+  console.log(a*3);
+} // 이게 함수 선언
+
+// 화살표 함수는 익명함수임!!
