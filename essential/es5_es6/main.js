e07781997736df2f144aec4f870e8f859297219f
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-12 22:58:06 +0900

    update 함수 복습

--- essential/es5_es6/main.js
+++ essential/es5_es6/main.js
@@ -1,17 +1,7 @@
-// 형 변환(Type conversion)
+// 함수 복습
 
-const a = 2
-const b = '2'
-
-console.log(a == b); // '==' 는 동등연산자
-                    // 자동으로 형변환 시킨다.
-
-// 참 같은 값 (Truthy)
-// true, {}, [], 1, 2, 'false', -12, '3.14' ...
-// 거짓 같은 값 (Falsy)
-// false, '', null, undefined, 0, -0, Nan
-
-// javaScript에서 사용되는 기능
-if (0) {
-  console.log('hoho')
+function sum() {
+  console.log(arguments);
+  return arguments[0] + arguments[1]
 }
+
