e0913f314709aefb3566ae9e9a88088920472817
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-12 21:44:28 +0900

    update 비교와 논리 연산자

--- essential/es5_es6/main.js
+++ essential/es5_es6/main.js
@@ -1,14 +1,35 @@
 'use strict'
-// 산술 연산자(arithmetic operator)
 
-console.log(1 + 2);
-console.log(5 - 7);
-console.log(5 * 7);
-console.log(5 / 7);
-console.log(5 % 7);
-console.log(5 / 7);
+// 비교 연산자(comparison operator)
+const a = 1
+const b = 2
 
-//  할당 연산자(assignment operator)
-let a = 4
-a /= 2
-console.log(a);
+console.log(a === b) // 일치 연산자 데이터의 값과 타입을 동시에 확인함.
+
+function isEqual(x, y) {
+  return  x === y
+}
+
+console.log(isEqual(1,1));
+console.log(isEqual(2,'2'));
+console.log(isEqual(1,1));
+
+
+const c = 1
+const d = 2
+
+console.log(c !== d);
+
+//논리 연산자(logical operator)
+
+const e = 1 === 1
+const f = 'AB' === 'AB'
+const g = false
+console.log(e);
+console.log(f);
+console.log(g);
+
+console.log('&&: ', e && f && g);
+console.log('||: ', e || f || g);
+console.log('!: ', !e);
+
