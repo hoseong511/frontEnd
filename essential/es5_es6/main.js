1aecd083d8597d3129a41f35c4f53644f0089cc6
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-12 21:15:39 +0900

    update js를 export 후 import 하는 법

--- essential/es5_es6/main.js
+++ essential/es5_es6/main.js
@@ -1,1 +1,19 @@
-console.log("hello!");
+'use strict'
+import getType from './getType'
+
+// 데이터의 타입을 알아내는 방법
+console.log(typeof "hello!");
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+
+// typeof와 아래방식의 차이를 확인하기
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType(null))
+console.log(getType({}))
+console.log(getType([]))
