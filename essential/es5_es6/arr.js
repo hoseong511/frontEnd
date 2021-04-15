000a0640499c5754cb26bfcb7c47d91bf4a06022
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-15 21:54:25 +0900

    update arr 함수 2

--- essential/es5_es6/arr.js
+++ essential/es5_es6/arr.js
@@ -1,4 +1,4 @@
-// .length .concat .forEach()
+// .length .concat .forEach() .filter()
 
 const numbers = [1,2,3,4]
 const fruits = ['Apple', 'Banana', "Cherry"]
@@ -13,7 +13,7 @@ fruits.forEach(function (element, index, array) {
   console.log(element, index, array);
 })
 
-const a = fruits.forEach(function (fruit, index) {
+let a = fruits.forEach(function (fruit, index) {
   console.log(`${fruit}-${index}`);
 })
 console.log(a);
@@ -24,9 +24,57 @@ console.log(a);
 //     name: fruit
 //   }
 // })
-const b = fruits.map((fruit, index) => ({
+let b = fruits.map((fruit, index) => ({
   id: index,
   name: fruit
 }))
 console.log(b);// 화살표함수를 이용할 때 return 이용법
 
+// ------------ map, filter 새로운 배열을 반환함!
+a = numbers.map(number => number < 3);
+console.log(a);
+
+b = numbers.filter(number => number < 3);
+console.log(b);
+
+// .find() .findIndex()
+a = fruits.find(fruit => /^B/.test(fruit)); // 정규표현식
+console.log(a);
+
+b = fruits.findIndex(fruit =>/^B/.test(fruit));
+console.log(b);
+
+// .includes()
+
+a = numbers.includes(3)
+console.log(a);
+
+b = fruits.includes('he')
+console.log(b);
+
+// .push() .inshift()
+
+numbers.push(5)
+console.log(numbers);
+
+numbers.unshift(0);
+console.log(numbers);
+
+// .reverse()
+
+numbers.reverse()
+fruits.reverse()
+
+console.log(numbers);
+console.log(fruits);
+
+// .splice()
+
+numbers.splice(2,1)
+console.log(numbers);
+
+numbers.splice(2, 1, 88);
+console.log(numbers);
+
+fruits.splice(2,0,'Orange');
+console.log(fruits);
