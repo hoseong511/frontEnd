8c1305719a7e7f2c9de02e20d32e459b0699d726
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-16 12:53:09 +0900

    update import/export 하는 방법

--- essential/es5_es6/getRandom.js
+++ essential/es5_es6/getRandom.js
@@ -1,5 +1,11 @@
-function random() {
+export function random() {
   return Math.floor(Math.random() * 10)
 }
 
-export default random
+export const user = {
+  name: 'Ho',
+  age: 25
+}
+
+// 이름을 지정한 내보내기는 여러개를 내보내기가 가능함.
+
