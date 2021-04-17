c66b215d9cf22bd95b9c02abc01888881863b075
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-17 15:20:12 +0900

    update regExp 패턴2

--- essential/regexp/main.js
+++ essential/regexp/main.js
@@ -1,27 +1,44 @@
 let str = ` 
-010-1234-1234.
+010-1234-1234._
 thdghtjd115@naver.com
 https://www.omdbapi.com/?apikey=12the312312&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbbbbbbbbccccccccccccddddddddd
 http://localhost:1234
-d` // 와우 백틱 쓰면 엔터 입력이 가능하네 \n 이거 안써도 되는구나.
+d_
+동해물과 백두산이 마르고 닳도록` // 와우 백틱 쓰면 엔터 입력이 가능하네 \n 이거 안써도 되는구나.
 //이스케이프 문자 \을 이용해서 본래의 기능에서 벗어나 상태가 바뀌는 문자를 의미 
 console.log(
-  str.match(/fox|dog/g)
+  str.match(/[가-힣]{1,}/g)
+);
+// console.log(
+//   str.match(/\w/g)
+// );
+console.log(
+  str.match(/\bf\w{1,}\b/g) // 소문자 f로 시작하는 영단어 찾기
 );
 console.log(
-  str.match(/h..p/g)
+  str.match(/\d{1,}/g) //숫자 덩어리!
 );
+
+
 console.log(
-  str.match(/https?/g) // a? a가 포함된 문자열 or 포함x 문자열
+  str.match(/\s/g)
 );
+
+const h = `   the hello world   !
+  
+  `
+
 console.log(
-  str.match(/d{2,}/g)
+  h.replace(/\s/g, '')
 );
+
+///////// 일치
+
 console.log(
-  str.match(/\w{2,3}/g)
-); // 숫자/알파벳이 연속되는 문자열을 모두 찾음
+  str.match(/.{1,}(?=@)/g)
+);  
 console.log(
-  str.match(/\b\w{2,3}\b/g) //경계값으로 찾음.
-);
+  str.match(/(?<=@).{1,}/g)
+);  
