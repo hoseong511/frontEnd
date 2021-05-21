3d106a2cacdd062a63a6bb8686aab25751c166ec
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-21 19:58:32 +0900

    update 6.static설정

--- essential/bundler/webpack-test/webpack.config.js
+++ essential/bundler/webpack-test/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 //export
 module.exports = {
   // 파일을 읽어들이기 시작하는 진입점
@@ -7,5 +10,19 @@ module.exports = {
     // path: path.resolve(__dirname, 'dist'), // output의 path는 절대경로를 사용!
     // filename: 'main.js', // default-> dist
     clean: true
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static'}
+      ]
+    })
+  ],
+  devServer: {
+    host: 'localhost'
   }
 } 
