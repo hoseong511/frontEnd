9f69160862a70af4f2b9d1e2e2ee0ce24aed7ace
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-28 15:57:33 +0900

    update Front: css 반응형

--- HTML/re/main.js
+++ HTML/re/main.js
@@ -13,3 +13,23 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');  
 });
+
+new Swiper('.project .swiper-container', {
+  // horizontal은 기본값.
+  // direction : 'horizontal',
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides : true, // 1번 슬라이드가 가운데 보이기
+  loop : true,
+  autoplay : {
+    delay : 5000
+  },
+  pagination : {
+    el: '.project .swiper-pagination',
+    clickable : true
+  },
+  navigation : {
+    prevEl : '.project .swiper-prev',
+    nextEl : '.project .swiper-next'
+  }
+});
